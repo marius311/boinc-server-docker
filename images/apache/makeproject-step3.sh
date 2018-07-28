3a78052549cbd4004af4428a89d332912c185600
marius <mmillea@ucdavis.edu>
2018-07-28 14:45:14 +0200

    update -b2d to work with new setup as well

--- images/apache/makeproject-step3.sh
+++ images/apache/makeproject-step3.sh
@@ -10,9 +10,9 @@ do
     # the first time we build a project, we wait here until the makeproject-step2.sh
     # script is done
     while [ ! -f .built_${PROJECT} ] ; do sleep 1; done
-    
 
     echo "Finalizing project startup..."
+    
     ln -sf ${PROJECT_ROOT}/${PROJECT}.httpd.conf /etc/apache2/sites-enabled/
     
     # if apache already booted up, restart it so as to reread the httpd.conf
@@ -27,6 +27,13 @@ do
         bin/start
         (echo "PATH=$PATH"; echo "SHELL=/bin/bash"; cat *.cronjob) | crontab
     """
+    
+    # gives $BOINC_USER permission to run Docker commands
+    DOCKER_GID=$(stat -c '%g' /var/run/docker.sock)
+    addgroup -gid ${DOCKER_GID} docker
+    addgroup ${BOINC_USER} docker
+    
+    echo "Project startup complete."
     
     # subsequent times we build a project (such as after a PROJECT change), we
     # go through once then possibly go through again to avoid a race condition
